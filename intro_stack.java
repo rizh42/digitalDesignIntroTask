1ff68f989159c2850665a4d39276c309af26faf5
Egor <egorvlz2013@yandex.ru>
2021-03-21 19:40:09 +0300

    Works only for strings w\o nesting (ex: 4[xy]3[z])

---
+++ intro_stack.java
@@ -0,0 +1,74 @@
+import java.util.regex.*;
+
+public class intro1 {
+
+    public static boolean verify(String str){
+        int bracketCount = 0;
+
+        for (int i = 0; i < str.length(); i++){
+            if (Character.isDigit(str.charAt(i))){
+                if (str.charAt(i + 1) != '['){
+                    return false;
+                }
+            }
+
+            if (str.charAt(i) == '['){
+                bracketCount++;
+            }
+
+            if (str.charAt(i) == ']'){
+                bracketCount--;
+            }
+        }
+
+        if (bracketCount != 0){
+            return false;
+        }
+
+        return (Pattern.matches("[a-zA-Z\\[\\]0-9]+", str));
+    }
+
+    public static String parse(String str){
+        String res = "";
+        int count = 0;
+        int bracketCount = 0;
+        int firstBracketInd = 0;
+
+        if (verify(str)){
+            for (int i = 0; i < str.length(); i++){
+                char currChar = str.charAt(i);
+                if (Character.isLetter(currChar) && bracketCount == 0){
+                    res += currChar;
+                }
+
+                if (Character.isDigit(currChar) && bracketCount == 0){
+                    count = currChar - '0';
+                }
+
+                if (currChar == '['){
+                    bracketCount++;
+                    if (bracketCount == 1){
+                        firstBracketInd = i;
+                    }
+                }
+
+                if (currChar == ']'){
+                    bracketCount--;
+                    if (bracketCount == 0){
+                        res += str.substring(firstBracketInd + 1, i).repeat(count);
+                    }
+                    firstBracketInd = 0;
+                }
+            }
+        }
+        else {
+            return "Bad string";
+        }
+        return res;
+    }
+
+    public static void main(String[] args) {
+        String str = "3[xy2[A4[g]]]";
+        System.out.println(parse(str));
+    }
+}
